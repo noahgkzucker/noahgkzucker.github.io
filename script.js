d249686bd8e402ad1e1883e213ceb8cf3eba818b
noahgkzucker <154071518+noahgkzucker@users.noreply.github.com>
2025-05-14 22:17:14 -0400

    Full commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // JavaScript is not strictly needed here since animations are handled in CSS.
+    // This script is included in case additional dynamic behavior is added later.
+  });
